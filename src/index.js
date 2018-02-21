efdb8e60ae5cc5cff9cd69aab60bdc6d1b680ac2
Andrew Kud <kud@mm.st>
2018-02-21 11:00:00 +0200

    Create redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import * as reducers from './store/reducers'
+
+const store = createStore(combineReducers(reducers))
+
+ReactDOM.render(
+  <Provider store={store}> 
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
