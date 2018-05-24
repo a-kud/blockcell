830dcf6f79d1e00fd5e8e820e671fdc9a3ffcc7b
Andrew Kud <kud@mm.st>
2018-05-24 11:00:00 +0300

    Display Dungeon in main app screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+import Dungeon from './containers/Dungeon'
 
 class App extends Component {
   render() {
     return (
-      <h1>Blockcell Game</h1>
+      <div className='App'>
+        <h1>Blockcell Game</h1>
+        <Dungeon />
+      </div>   
     );
   }
 }
